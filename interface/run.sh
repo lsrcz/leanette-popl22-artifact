2d46e467cb669b1ace36b9930fb71b36bb6241a7
Sirui Lu <siruilu@cs.washington.edu>
2022-10-05 01:13:52 -0700

    feat: sanity check and formatting

--- interface/run.sh
+++ interface/run.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-raco cross --workspace "$parent_path/../rosette-4" racket line-count.rkt rosette-4
-raco cross --workspace "$parent_path/../rosette-3" racket line-count.rkt rosette-3
-raco cross --workspace "$parent_path/../rosette-4" racket report.rkt
+echo "---- Rosette 4 ----"
+raco cross -q --workspace "$parent_path/../rosette-4" racket line-count.rkt rosette-4
+echo "---- Rosette 3 ----"
+raco cross -q --workspace "$parent_path/../rosette-3" racket line-count.rkt rosette-3
